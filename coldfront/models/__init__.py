d942c0e2b79ad6cbc6c20488e01a2f32558f2ab0
Andrew E. Bruno <aebruno2@buffalo.edu>
2026-02-17 12:59:10 -0500

    Add ColdFront base models.
    - Add base models
    - Add Django MPTT for use with storing hierarchical models
    
    Signed-off-by: Andrew E. Bruno <aebruno2@buffalo.edu>

---
+++ coldfront/models/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: (C) ColdFront Authors
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .base import ColdFrontModel, NestedGroupModel, OrganizationalModel, PrimaryModel
+
+__all__ = (
+    "ColdFrontModel",
+    "PrimaryModel",
+    "OrganizationalModel",
+    "NestedGroupModel",
+)
---
+++ coldfront/models/base.py
@@ -0,0 +1,161 @@
+# SPDX-FileCopyrightText: (C) ColdFront Authors
+# SPDX-FileCopyrightText: (C) DigitalOcean, LLC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later AND Apache-2.0
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import ValidationError
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
+
+from coldfront.querysets import TreeManager
+from coldfront.users.querysets import RestrictedQuerySet
+
+
+class BaseModel(models.Model):
+    """
+    A global base model for all ColdFront objects.
+
+    This class provides some important overrides to Django's default functionality, such as
+    - Overriding the default manager to use RestrictedQuerySet
+    - Extending `clean()` to validate GenericForeignKey fields
+    """
+
+    objects = RestrictedQuerySet.as_manager()
+
+    class Meta:
+        abstract = True
+
+    def clean(self):
+        """
+        Validate the model for GenericForeignKey fields to ensure that the content type and object ID exist.
+        """
+        super().clean()
+
+        for field in self._meta.get_fields():
+            if isinstance(field, GenericForeignKey):
+                ct_value = getattr(self, field.ct_field, None)
+                fk_value = getattr(self, field.fk_field, None)
+
+                if ct_value is None and fk_value is not None:
+                    raise ValidationError(
+                        {
+                            field.ct_field: "This field cannot be null.",
+                        }
+                    )
+                if fk_value is None and ct_value is not None:
+                    raise ValidationError(
+                        {
+                            field.fk_field: "This field cannot be null.",
+                        }
+                    )
+
+                if ct_value and fk_value:
+                    klass = getattr(self, field.ct_field).model_class()
+                    try:
+                        obj = klass.objects.get(pk=fk_value)
+                    except ObjectDoesNotExist:
+                        raise ValidationError(
+                            {field.fk_field: f"Related object not found using the provided value: {fk_value}."}
+                        )
+
+                    # update the GFK field value
+                    setattr(self, field.name, obj)
+
+
+class ColdFrontModel(BaseModel):
+    """
+    Base model for most object types. Suitable for use by plugins.
+    """
+
+    class Meta:
+        abstract = True
+
+
+class PrimaryModel(ColdFrontModel):
+    """
+    Primary model used for internal ColdFront functionality
+    """
+
+    class Meta:
+        abstract = True
+
+
+class OrganizationalModel(ColdFrontModel):
+    """
+    Organizational models are those which are used solely to categorize and qualify other objects.
+    """
+
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=100,
+        unique=True,
+    )
+    slug = models.SlugField(
+        verbose_name=_("slug"),
+        max_length=100,
+        unique=True,
+    )
+    description = models.CharField(
+        verbose_name=_("description"),
+        max_length=200,
+        blank=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class NestedGroupModel(ColdFrontModel, MPTTModel):
+    """
+    Base model for objects which are used to form a hierarchy (tenants, roles, etc.). These models nest
+    recursively using MPTT. Within each parent, each child instance must have a unique name.
+    """
+
+    parent = TreeForeignKey(
+        to="self",
+        on_delete=models.CASCADE,
+        related_name="children",
+        blank=True,
+        null=True,
+        db_index=True,
+    )
+    name = models.CharField(
+        verbose_name=_("name"),
+        max_length=100,
+    )
+    slug = models.SlugField(
+        verbose_name=_("slug"),
+        max_length=100,
+    )
+    description = models.CharField(
+        verbose_name=_("description"),
+        max_length=200,
+        blank=True,
+    )
+
+    objects = TreeManager()
+
+    class Meta:
+        abstract = True
+
+    class MPTTMeta:
+        order_insertion_by = ("name",)
+
+    def __str__(self):
+        return self.name
+
+    def clean(self):
+        super().clean()
+
+        # An MPTT model cannot be its own parent
+        if not self._state.adding and self.parent and self.parent in self.get_descendants(include_self=True):
+            raise ValidationError(
+                {"parent": "Cannot assign self or child {type} as parent.".format(type=self._meta.verbose_name)}
+            )
