e9a920453d01899b87f4cc512d773c00bceb2ed6
Andrew E. Bruno <aebruno2@buffalo.edu>
2026-07-24 14:35:24 -0400

    Add custom fields to filterset forms
    Signed-off-by: Andrew E. Bruno <aebruno2@buffalo.edu>

--- coldfront/forms/filterset_forms.py
+++ coldfront/forms/filterset_forms.py
@@ -6,7 +6,10 @@ from crispy_forms.layout import Fieldset, Layout
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from .mixins import HorizontalFormMixin, SavedFiltersMixin
+from coldfront.core.choices import CustomFieldFilterLogicChoices
+from coldfront.core.models import CustomField, ObjectType
+
+from .mixins import CustomFieldsMixin, HorizontalFormMixin, SavedFiltersMixin
 
 __all__ = (
     "ColdFrontModelFilterSetForm",
@@ -16,7 +19,7 @@ __all__ = (
 )
 
 
-class BaseModelFilterSetForm(SavedFiltersMixin, HorizontalFormMixin, forms.Form):
+class BaseModelFilterSetForm(SavedFiltersMixin, HorizontalFormMixin, CustomFieldsMixin, forms.Form):
     """
     Base form for FilerSet forms. These are used to filter object lists in the ColdFront UI. Note that the
     corresponding FilterSet *must* provide a `q` filter.
@@ -35,11 +38,53 @@ class BaseModelFilterSetForm(SavedFiltersMixin, HorizontalFormMixin, forms.Form)
         helper.form_method = "get"
         return helper
 
+    def _get_content_type(self):
+        return ObjectType.objects.get_for_model(self.model)
+
+    def _get_custom_fields(self, content_type):
+        fields = []
+
+        for cf in CustomField.objects.get_for_model(content_type.model_class()):
+            # Include only custom fields that are enabled for filtering
+            if cf.filter_logic == CustomFieldFilterLogicChoices.FILTER_DISABLED:
+                continue
+            fields.append(cf)
+
+        return fields
+
+    def _get_form_field(self, customfield):
+        return customfield.to_form_field(
+            set_initial=False,
+            enforce_required=False,
+            enforce_visibility=False,
+            for_filterset_form=True,
+        )
+
     def get_layout(self):
         """
         Override crispy layout to include search and filter fields
         """
         fieldsets = [Fieldset(_("Search"), "q", "filter_id"), *self.fieldsets]
+
+        # Add custom fields section if any custom fields exist
+        if self.custom_fields:
+            custom_fieldsets = []
+            for group, fields in self.custom_field_groups.items():
+                if group:
+                    custom_fieldsets.append(
+                        Fieldset(
+                            group,
+                            *fields,
+                        )
+                    )
+                else:
+                    custom_fieldsets.append(
+                        Fieldset(
+                            _("Custom Fields"),
+                            *fields,
+                        )
+                    )
+            fieldsets.extend(custom_fieldsets)
 
         return Layout(*fieldsets)
 
